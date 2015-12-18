85bb750a21c5cfd8cd14928278436a46f883c3eb
MrMendonez <dmendonez@gmail.com>
2015-12-18 13:47:26 -0500

    got collapsed menu working by adding necessary sideNav js function. Also styled sideNave so it aligns icons vertically

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Scripts for Daily Briefing
+
+$(document).ready(function(){
+
+  // To initiate side Nav for mobile devices
+  $(".button-collapse").sideNav({ 
+    menuWidth: 100 // So icons appear vertically instead of horizontally
+  });
+
+}); // End document ready function
