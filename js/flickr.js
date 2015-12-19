2754db6917211ff1b0c57256147d22f1d978017d
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2015-12-19 13:53:27 -0500

    Added flickr.js

---
+++ js/flickr.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {  
+  $(".btn-default").on("click", function(e){
+    e.preventDefault();   // prevents the default button event
+
+
+  function buildThumbnail(photoData) {  //function to show thumbnails into HTML page
+    var photoUrl = "https://farm" + photoData.farm; // assign photoUrl 
+    photoUrl += ".staticflickr.com/" + photoData.server;
+    photoUrl += "/" + photoData.id;
+    photoUrl += "_" + photoData.secret + ".jpg";
+
+    var colDiv = $("<div>").addClass("col-md-3");
+    var thumbnailDiv = $("<div>").addClass("thumbnail");
+    var photoImg = $("<img>").attr("src", photoUrl);
+    var captionDiv = $("<div>").addClass("caption");
+    var picTitle = $("<p>").append(photoData.title);
+
+    colDiv.append(thumbnailDiv
+      .append(photoImg)
+      .append(captionDiv
+        .append(picTitle)
+      )
+    );
+
+    return colDiv;
+
+  }
+
+  function googleApiSuccessHandler(response) {
+    
+
+    
+    var flickrApiUrl = "https://api.flickr.com/services/rest/?";
+    var flickrApiParams = {
+      api_key: "4878be0a0e72be523088855dc3771f33",
+      method: "flickr.photos.search",
+      format: "json",
+      nojsoncallback: 1,
+      lat: geoLocation.lat,
+      lon: geoLocation.lng
+    }
+    
+    $.ajax({
+      type: "GET",
+      url: flickrApiUrl + $.param(flickrApiParams),
+      success: flickrSuccessHandler
+    });
+  }
+
+  function flickrSuccessHandler(response) { //function running because the AJAX request was successful
+    var locationPhotos = response.photos.photo; // assigning the results from AJAX request into locationPhones
+    for(var i = 0; i < locationPhotos.length; i++) {  // passing
+      var newCol = buildThumbnail(locationPhotos[i]);
+      $("#photosRow").append(newCol);
+    }
+  }
+});
