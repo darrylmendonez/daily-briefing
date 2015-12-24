b0ab98a88f6674a590b08ceca84f75c36891864b
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-24 00:40:17 -0500

    disabled scrollwheel on google map

--- js/main.js
+++ js/main.js
@@ -111,7 +111,8 @@ $(document).ready(function(){
   function initMap(geoLocation) {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 12,
-      center: geoLocation
+      center: geoLocation,
+      scrollwheel: false
     });
     var trafficLayer = new google.maps.TrafficLayer();
     trafficLayer.setMap(map);
