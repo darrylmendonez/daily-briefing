cf8392c9f2417ca71ec7a64e6e857592343ad033
Paul Santos <paulmichael.a.santos@gmail.com>
2015-12-23 19:04:16 -0500

    still trying to figure out how to get the timezones to appear correctly

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
 
-  var date = new Date();
-  var year = date.getFullYear();
-  var month = date.getMonth() + 1;
-  var hour = date.getHours()*3600;
-  var day = date.getDate();
-  var minutes = date.getMinutes()*60;
-  var seconds = date.getSeconds();
-  var currentTime = hour + minutes + seconds;
+  $("#selected-city").keypress(function(e){
+    if (e.which == 13) {
+      setTimeout(function(){
+        getTimeZoneOfCity();
+      }, 1000)
+    }
+  });
   
 
   // clock settings
@@ -16,39 +15,60 @@ $(document).ready(function(){
     clockFace: "TwelveHourClock"
   });
 
-  localClock.start(function(){
-
-  });
-  setTimeout(function(){
-    getTimeZoneOfCity();
-  }, 10000)
   
+
+  //start the clock for the user's timezone
+  localClock.start(function(){});
+  
+  var now_utc;
   function getTimeZoneOfCity (){
     var now = new Date();
     var now_utc = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-    now_utc = now_utc/1000;
+    now_utc = now_utc;
+    console.log(now_utc*1000);
     var lat = geoLocation.lat;
     var lng = geoLocation.lng;
     var googleTimezoneApiURL = "https://maps.googleapis.com/maps/api/timezone/json?"
     googleTimezoneApiURL += "location=" + lat + "," + lng;
-    googleTimezoneApiURL += "&timestamp=" + now_utc + "&key=AIzaSyDZQURt4o0BU3APcPrl91bvkQOYwCo99Lg&sensor=false"
+    googleTimezoneApiURL += "&timestamp=" + now_utc/1000 + "&key=AIzaSyDZQURt4o0BU3APcPrl91bvkQOYwCo99Lg&sensor=false"
 
     $.ajax({
       type: "GET",
       url: googleTimezoneApiURL,
       success: function(timezoneData){
-        console.log(timezoneData);
+        locationTime(timezoneData.timeZoneId);
       }
       
     });
   }
 
+  function locationTime (timeZone){
+    // var localOffset = new Date().getTimezoneOffset();
+    // console.log(localOffset);
+    // // var newTime = new Date(moment().tz(timeZone).format("MMM DD, YYYY HH:MM:SS"));
+    // var newOffset = moment.tz.zone(timeZone).offset();
+    // console.log(newOffset);
+    // var newOffset = newTime.getTimezoneOffset();
+    // console.log(newOffset);
+    // var diff = localOffset - newOffset;
+    // console.log(diff);
+    // var locationClock = $(".location-clock").FlipClock(diff, {
+    //   clockFace: "TwelveHourClock"
+    // });
+
+    var newOffset = moment.tz(timeZone).format("Z");
+    console.log(typeof(newOffset));
+
+    $("#flipcountdownbox1").flipcountdown({
+      size: "lg",
+      tzoneOffset: newOffset
+    });
+  }
 
 
-
-
+  
 
 });
 
-
+ var newOffset;
 // google timezone api key: AIzaSyDZQURt4o0BU3APcPrl91bvkQOYwCo99Lg
