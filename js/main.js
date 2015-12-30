3281443989f593d26cf22fd2dd249bb9d344ba13
MrMendonez <dmendonez@gmail.com>
2015-12-30 02:29:48 -0500

    changed font color of weather block to make it eligible for all weather images

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ $(document).ready(function(){
     var data;
     $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
       $('#weather').html("Weather Summary: " + data.currently.summary + "<br /> Current Temperature: " + data.currently.temperature.toFixed(0) + "&deg; F");
-      var skycons = new Skycons({"color": "#1976D2"});
+      var skycons = new Skycons({"color": "#1A237E"});
       var currentWeatherIcon = data.currently.icon;
       console.log("var currentWeatherIcon = " + currentWeatherIcon);
 
