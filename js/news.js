38094f0bbba1840305278907078c8194ca45cd47
Paul Santos <paulmichael.a.santos@gmail.com>
2016-01-04 13:51:47 -0500

    reverted back because global functions not working

--- js/news.js
+++ js/news.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
 /*================================================================
     BING API AJAX 
   ================================================================ */
-  window.bingNewsAPI = function (){
+  function bingNewsAPI(){
     $("#news").empty();
     var city = $("#selected-city").val().trim(); //get the city inputted
 
