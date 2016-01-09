56b7a56b7a28046844452ce3b6a8dd62c6d0b1ce
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-08 21:08:45 -0500

    Fixed icon color

--- js/main.js
+++ js/main.js
@@ -228,7 +228,7 @@ $(document).ready(function(){
     var data;
     $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
       $("#weather").html("&nbsp;&nbsp;Today: " + data.currently.summary + "<br />&nbsp;&nbsp;Currently: " + data.currently.temperature.toFixed(0) + "&deg; F" + "<br />&nbsp;&nbsp;Min: " + data.daily.data[0].temperatureMin.toFixed(0) + "&deg; F" + "<br />&nbsp;&nbsp;Max: " + data.daily.data[0].temperatureMax.toFixed(0) + "&deg; F");
-      var skycons = new Skycons({"color": "#E65100"});
+      var skycons = new Skycons({"color": "#f57c00"});
       var currentWeatherIcon = data.currently.icon;
       $("#weather-tomorrow").html("&nbsp;&nbsp;Tomorrow: " + data.daily.data[1].summary + "<br />&nbsp;&nbsp;Min: " + data.daily.data[1].temperatureMin.toFixed(0) + "&deg; F" + "<br />&nbsp;&nbsp;Max: " + data.daily.data[1].temperatureMax.toFixed(0) + "&deg; F");
       console.log(data);
